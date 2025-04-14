53254b11e9d4f7cb8c427e4a8ed0c634ed208256
Yoav Levy <yoavlevy75@gmail.com>
2025-04-14 21:48:34 +0300

    basically works

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"  # type: ignore
 )
 
-K = 20
+K = 25
 
-center_kalman_filter = PointKalmanFilter(K)
 rect_kalman_filter = RectangleKalmanFilter(K)
 
 while True:
@@ -39,11 +38,11 @@ while True:
 
     if len(faces_list) > 0:
         rect_kalman_filter.update(faces_list[0])
-        center_kalman_filter.update(faces_list[0].get_center())
 
     face = rect_kalman_filter.get()
 
     corners = face.get_corners()
+    center = face.get_center()
 
     cv2.rectangle(
         frame,
@@ -53,9 +52,7 @@ while True:
         4,
     )
 
-    center = center_kalman_filter.get()
-
-    cv2.circle(frame, (int(center.x), int(center.y)), 5, (0, 0, 255), 10)
+    cv2.circle(frame, center.as_tuple(), 5, (0, 0, 255), 10)
 
     OFFSET = 50  # Pixels
     RES = 16 / 9
@@ -65,16 +62,38 @@ while True:
 
     top_y_cropped_frame = max(corners.top_left.y - OFFSET, 0)
     bottom_y_cropped_frame = min(corners.bottom_right.y + OFFSET, frame_height)
-    cropped_height = bottom_y_cropped_frame - top_y_cropped_frame
 
-    left_x_cropped_frame = max(corners.top_left.x - OFFSET, 0)
-    right_x_cropped_frame = min(
-        left_x_cropped_frame + int(cropped_height * RES),
-        frame_width,
+    cropped_height = bottom_y_cropped_frame - top_y_cropped_frame
+    cropped_width = int(cropped_height * RES)
+
+    exceeds_left = int(center.x - cropped_width / 2) < 0
+    exceeds_right = int(center.x + cropped_width / 2) > frame_width
+
+    left_x_cropped_frame = 0
+    right_x_cropped_frame = frame_width
+
+    if exceeds_left:
+        right_x_cropped_frame = min(cropped_width, frame_width)
+    elif exceeds_right:
+        left_x_cropped_frame = max(frame_width - cropped_width, 0)
+    else:
+        left_x_cropped_frame = int(center.x - cropped_width / 2)
+        right_x_cropped_frame = int(center.x + cropped_width / 2)
+
+    cv2.line(
+        frame,
+        (center.x, top_y_cropped_frame),
+        (center.x, bottom_y_cropped_frame),
+        (255, 255, 0),
+        4,
     )
 
-    print(
-        f"ty: {top_y_cropped_frame}, by: {bottom_y_cropped_frame}, H: {cropped_height}, hr: {cropped_height * RES}, lx: {left_x_cropped_frame}, rx: {right_x_cropped_frame}"
+    cv2.line(
+        frame,
+        (int(center.x - cropped_width / 2), center.y),
+        (int(center.x + cropped_width / 2), center.y),
+        (255, 255, 0),
+        4,
     )
 
     cropped_frame = frame[
@@ -82,8 +101,9 @@ while True:
         left_x_cropped_frame:right_x_cropped_frame,
     ]
 
+    resized = cv2.resize(cropped_frame, (1920, 1080))
     cv2.imshow("Webcam Feed", frame)  # Show the frame
-    cv2.imshow("Cropped Feed", cropped_frame)  # Show the frame
+    cv2.imshow("Cropped Feed", resized)  # Show the frame
 
     # Press 'q' to exit
     if cv2.waitKey(1) == ord("q"):
